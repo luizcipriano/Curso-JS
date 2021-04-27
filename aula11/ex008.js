0293b7a3af454d4af366b395ccba668656fa7fe7
Luiz Antônio de Araújo Cipriano <77516067+luizcipriano@users.noreply.github.com>
2021-04-27 19:33:18 -0300

    27/04 pt1

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}KM/h` )
+if(vel > 60){
+    console.log(`Você utrapassou a velocidade maxima. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Alemanhã'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil'){
+    console.log('Você é Brasileiro')
+}else{
+    console.log('Você é Gringo')
+}
