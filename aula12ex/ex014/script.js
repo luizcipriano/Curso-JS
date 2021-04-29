2b8f6d7927d33df38ecf7745de1bd88164094cdf
Luiz Antônio de Araújo Cipriano <77516067+luizcipriano@users.noreply.github.com>
2021-04-29 01:56:11 -0300

    modelo Bom dia/tarde/noite

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.toLocaleTimeString()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 5 && hora < 12) {
+        //bom dia
+        img.src = 'fotomanha.png'
+        document.body.style.background ='#c9b70f'
+    } else if (hora >= 12 && hora < 18) {
+        //boa tarde
+        img.src = 'fototarde.png'
+        document.body.style.background ='#d4977e'
+    }else {
+        //boa noite
+        img.src = 'fotonoite.png'
+        document.body.style.background ='#0f2739'
+    }
+}
+var relogio = setInterval(() => {carregar()},1000);
