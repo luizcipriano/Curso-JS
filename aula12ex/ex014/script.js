abb6f2e4e49f612f282bd4996a0a52e54b0ac5f3
Luiz Antônio de Araújo Cipriano <77516067+luizcipriano@users.noreply.github.com>
2021-05-01 17:35:55 -0300

    01/05

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,20 +2,22 @@ function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
-    var hora = data.toLocaleTimeString()
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 5 && hora < 12) {
+    var hora = data.getHours()
+   // var hora = data.toLocaleTimeString()
+   msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0  && hora < 12) {
         //bom dia
         img.src = 'fotomanha.png'
         document.body.style.background ='#c9b70f'
-    } else if (hora >= 12 && hora < 18) {
+    } else if (hora >= 12 && hora <= 18 ) {
         //boa tarde
         img.src = 'fototarde.png'
         document.body.style.background ='#d4977e'
-    }else {
+    }else  {
         //boa noite
         img.src = 'fotonoite.png'
         document.body.style.background ='#0f2739'
     }
 }
+
 var relogio = setInterval(() => {carregar()},1000);
---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log(`tudo bem ${c}`)
+    c++
+} while (c <= 6); // Teste logico no Final !!
+
+
+// var c = 1
+// while (c <= 6){
+//     console.log(`Tudo Bem ? ${c}`)
+//     c++
+// }//Teste logico no Inicio !!
+
+
+/*
+consolo.log('Tudo bem?')
+consolo.log('Tudo bem?')
+consolo.log('Tudo bem?')
+consolo.log('Tudo bem?')
+consolo.log('Tudo bem?')
+consolo.log('Tudo bem?')
+*/ //FORÇA BRUTA
